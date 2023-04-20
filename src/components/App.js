3ba819797ed94f9bd062a46b8e7c3a2a272da85c
Anupreet1213 <anupreet.2003@gmail.com>
2023-04-20 23:14:14 +0530

    Add Drag components

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import NavBar from "./NavBar/NavBar";
-import Hero from "./Hero";
-import Roadmap from "./Roadmap";
+import Hero from "./Hero/Hero";
+import Roadmap from "./Roadmap/Roadmap";
+import Feature from "./Feature/Feature";
+import Draggable from "./Drag/Draggable";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <Feature />
       <Roadmap />
+      <Draggable />
     </div>
   );
 }
